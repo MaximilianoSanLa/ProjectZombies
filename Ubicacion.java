ada155607f2b311ec6b81af12ceeaca59472cf80
MaximilianoSanLa <104327901+MaximilianoSanLa@users.noreply.github.com>
2022-04-24 19:47:34 -0400

    Update Ubicacion.java

--- Ubicacion.java
+++ Ubicacion.java
@@ -1,3 +1,28 @@
-class Ubicacion{
-  
+
+public class Ubicacion
+{  
+    private String nombre;
+    private float distanciaA;
+    private int cantidadZ;
+
+    public Ubicacion(String nombre, float distanciaA, int cantidadZ) {
+        this.nombre = nombre;
+        this.distanciaA = distanciaA;
+        this.cantidadZ = cantidadZ;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getDistanciaA() {
+        return distanciaA;
+    }
+
+    public int getCantidadZ() {
+        return cantidadZ;
+    }
+
+
 }
+

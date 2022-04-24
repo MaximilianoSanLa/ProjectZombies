71d7c4f561afa97fb6a6b7b18c72faba31b48a78
MaximilianoSanLa <104327901+MaximilianoSanLa@users.noreply.github.com>
2022-04-24 19:46:45 -0400

    Update Zombie.java

--- Zombie.java
+++ Zombie.java
@@ -1,3 +1,31 @@
-class Zombie{
 
+public class Zombie
+{
+    private String nombre;
+    private int salud;
+    private String fechaNacimiento;
+    private String tipoSangre;
+
+    public Zombie(String nombre, int salud, String fechaNacimiento, String tipoSangre) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fechaNacimiento = fechaNacimiento;
+        this.tipoSangre = tipoSangre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
 }
